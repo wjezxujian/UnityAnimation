6f9ec1baaff474f78a560e1ba310ca30b7091083
jian.xu <jian.xu@dena.com>
2018-06-26 19:14:38 +0800

    Add Character Animation

---
+++ Assets/Scripts/Biker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biker : MonoBehaviour 
+{
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxisRaw("Vertical");
+        anim.SetInteger("Vertical", (int)v);
+
+        transform.Translate(Vector3.forward * v * Time.deltaTime * 2.8f );
+	}
+}
